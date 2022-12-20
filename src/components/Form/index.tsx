416646fdf259ce23ee2fff5d4cc0b8612c85c4a8
Rafael Araujo <rafa123839@gmail.com>
2022-12-20 16:24:34 -0300

    implementado datepicker

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,16 +1,86 @@
-import { Container, DateAndTime, Input, Title } from "./styles";
+import { useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { ButtonDiet, Container, ContentButton, ContentData, DateAndTime, Hora, HourDateText, Input, InputDataHora, Status, Title } from "./styles";
+import { Button, Platform, Text } from "react-native";
 
 export default function Form() {
+    const [date, setDate] = useState(new Date())
+    const [mode, setMode] = useState();
+    const [show, setShow] = useState(false)
+    const [dateSelected, setDateSelected] = useState("")
+    const [hourSelected, setHourSelected] = useState("")
+    const [text, setText] = useState("Empty")
+
+    const onChange = (event: any, selectedDate: any) => {
+        const currentDate = selectedDate || date
+        setShow(Platform.OS === 'ios')
+        setDate(currentDate)
+
+        let tempDate = new Date(currentDate)
+        let fDate = tempDate.getDate() + "/" + (tempDate.getMonth() + 1) + "/" + tempDate.getFullYear()
+        let fTime = tempDate.getHours() + ':' + tempDate.getMinutes()
+        setText(fDate + "\n" + fTime)
+        setDateSelected(fDate)
+        setHourSelected(fTime)
+        console.log(fDate + ' ( ' + fTime + ")")
+    }
+
+    const showMode = (currentMode: any) => {
+        setShow(true);
+        console.log(currentMode)
+        setMode(currentMode);
+    };
+
     return (
         <Container>
             <Title>Nome</Title>
             <Input />
+
             <Title>Descrição</Title>
             <Input multiline numberOfLines={6} />
+
             <DateAndTime>
-                {/* <Input />
-                <Input/> */}
+                <ContentData>
+                    <Title>Data</Title>
+                    <InputDataHora onPress={() => showMode('date')}>
+                        <HourDateText>{dateSelected}</HourDateText>
+                    </InputDataHora>
+                </ContentData>
+
+                <ContentData>
+                    <Title>Hora</Title>
+                    <InputDataHora onPress={() => showMode('time')}>
+                        <HourDateText>{hourSelected}</HourDateText>
+                    </InputDataHora>
+                </ContentData>
+                {
+                    show && (
+                        <DateTimePicker
+                            testID="DateTimePicker"
+                            value={date}
+                            mode={mode}
+                            is24Hour={true}
+                            display="default"
+                            onChange={onChange}
+                        />
+                    )
+                }
+
             </DateAndTime>
-        </Container>
+
+            <Title>Esta dentro da dieta?</Title>
+            <ContentButton>
+                <ButtonDiet>
+                    <Status />
+                    <Text>Sim</Text>
+                </ButtonDiet>
+
+                <ButtonDiet>
+                    <Status />
+                    <Text>Não</Text>
+                </ButtonDiet>
+            </ContentButton>
+
+        </Container >
     )
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -22,4 +22,46 @@ export const Input = styled(TextInput)`
 
 export const DateAndTime = styled.View`
     flex-direction: row;
+    justify-content: space-between;
+`
+export const InputDataHora = styled.TouchableOpacity`
+    border-width: 1px;
+    border-color: ${({ theme }) => theme.COLORS.GRAY_500};
+    height: 50px;
+    border-radius: 6px;
+    justify-content: center;
+    align-items: center;
+`
+export const Hora = styled.TouchableOpacity`
+    border-width: 1px;
+    border-color: ${({ theme }) => theme.COLORS.GRAY_500};
+    height: 50px;
+    border-radius: 6px;
+ `
+export const HourDateText = styled.Text`
+    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+`
+export const ContentData = styled.View`
+    width: 47%;
+`
+export const ContentButton = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const ButtonDiet = styled.TouchableOpacity`
+    width: 47%;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    height: 50px;
+    border-radius: 6px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
+export const Status = styled.View`
+    width: 8px;
+    height: 8px;
+    background-color: ${({ theme }) => theme.COLORS.GREEN_DARK};
+    border-radius: 4px;
+    margin-right: 6px;
 `
